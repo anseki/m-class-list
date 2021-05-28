5a2f70182bfcadf3caca290068d59460d90639aa
anseki <AnSeki.aff@gmail.com>
2021-05-28 20:59:31 +0900

    Update for @babel/core@7

--- m-class-list-debug.esm.js
+++ m-class-list-debug.esm.js
@@ -9,10 +9,11 @@
  * Copyright (c) 2018 anseki
  * Licensed under the MIT license.
  */
-
 function normalize(token) {
   return (token + '').trim();
 } // Not `||`
+
+
 function applyList(list, element) {
   element.setAttribute('class', list.join(' '));
 }
@@ -22,6 +23,7 @@ function _add(list, element, tokens) {
     if (!(token = normalize(token)) || list.indexOf(token) !== -1) {
       return false;
     }
+
     list.push(token);
     return true;
   }).length) {
@@ -31,10 +33,12 @@ function _add(list, element, tokens) {
 
 function _remove(list, element, tokens) {
   if (tokens.filter(function (token) {
-    var i = void 0;
+    var i;
+
     if (!(token = normalize(token)) || (i = list.indexOf(token)) === -1) {
       return false;
     }
+
     list.splice(i, 1);
     return true;
   }).length) {
@@ -44,31 +48,39 @@ function _remove(list, element, tokens) {
 
 function _toggle(list, element, token, force) {
   var i = list.indexOf(token = normalize(token));
+
   if (i !== -1) {
     if (force) {
       return true;
     }
+
     list.splice(i, 1);
     applyList(list, element);
     return false;
   }
+
   if (force === false) {
     return false;
   }
+
   list.push(token);
   applyList(list, element);
   return true;
 }
 
 function _replace(list, element, token, newToken) {
-  var i = void 0;
+  var i;
+
   if (!(token = normalize(token)) || !(newToken = normalize(newToken)) || token === newToken || (i = list.indexOf(token)) === -1) {
     return;
   }
+
   list.splice(i, 1);
+
   if (list.indexOf(newToken) === -1) {
     list.push(newToken);
   }
+
   applyList(list, element);
 }
 
@@ -87,18 +99,20 @@ function mClassList(element) {
       },
       add: function add() {
         _add(list, element, Array.prototype.slice.call(arguments));
+
         return mClassList.methodChain ? ins : void 0;
       },
       remove: function remove() {
         _remove(list, element, Array.prototype.slice.call(arguments));
+
         return mClassList.methodChain ? ins : void 0;
       },
-
       toggle: function toggle(token, force) {
         return _toggle(list, element, token, force);
       },
       replace: function replace(token, newToken) {
         _replace(list, element, token, newToken);
+
         return mClassList.methodChain ? ins : void 0;
       }
     };
@@ -107,5 +121,4 @@ function mClassList(element) {
 }
 
 mClassList.methodChain = true;
-
 export default mClassList;
--- test/m-class-list.js
+++ test/m-class-list.js
@@ -103,10 +103,11 @@ __webpack_require__.r(__webpack_exports__);
  * Copyright (c) 2018 anseki
  * Licensed under the MIT license.
  */
-
 function normalize(token) {
   return (token + '').trim();
 } // Not `||`
+
+
 function applyList(list, element) {
   element.setAttribute('class', list.join(' '));
 }
@@ -116,6 +117,7 @@ function _add(list, element, tokens) {
     if (!(token = normalize(token)) || list.indexOf(token) !== -1) {
       return false;
     }
+
     list.push(token);
     return true;
   }).length) {
@@ -125,10 +127,12 @@ function _add(list, element, tokens) {
 
 function _remove(list, element, tokens) {
   if (tokens.filter(function (token) {
-    var i = void 0;
+    var i;
+
     if (!(token = normalize(token)) || (i = list.indexOf(token)) === -1) {
       return false;
     }
+
     list.splice(i, 1);
     return true;
   }).length) {
@@ -138,31 +142,39 @@ function _remove(list, element, tokens) {
 
 function _toggle(list, element, token, force) {
   var i = list.indexOf(token = normalize(token));
+
   if (i !== -1) {
     if (force) {
       return true;
     }
+
     list.splice(i, 1);
     applyList(list, element);
     return false;
   }
+
   if (force === false) {
     return false;
   }
+
   list.push(token);
   applyList(list, element);
   return true;
 }
 
 function _replace(list, element, token, newToken) {
-  var i = void 0;
+  var i;
+
   if (!(token = normalize(token)) || !(newToken = normalize(newToken)) || token === newToken || (i = list.indexOf(token)) === -1) {
     return;
   }
+
   list.splice(i, 1);
+
   if (list.indexOf(newToken) === -1) {
     list.push(newToken);
   }
+
   applyList(list, element);
 }
 
@@ -181,18 +193,20 @@ function mClassList(element) {
       },
       add: function add() {
         _add(list, element, Array.prototype.slice.call(arguments));
+
         return mClassList.methodChain ? ins : void 0;
       },
       remove: function remove() {
         _remove(list, element, Array.prototype.slice.call(arguments));
+
         return mClassList.methodChain ? ins : void 0;
       },
-
       toggle: function toggle(token, force) {
         return _toggle(list, element, token, force);
       },
       replace: function replace(token, newToken) {
         _replace(list, element, token, newToken);
+
         return mClassList.methodChain ? ins : void 0;
       }
     };
@@ -201,7 +215,6 @@ function mClassList(element) {
 }
 
 mClassList.methodChain = true;
-
 /* harmony default export */ __webpack_exports__["default"] = (mClassList);
 
 /***/ })
